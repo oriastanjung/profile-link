0459ec506d152ee85b7be63e0462bbf424dbb044
Admin <oriastan999@gmail.com>
2023-08-07 00:39:44 +0700

    backend using strapi

---
+++ src/admin/app.js
@@ -0,0 +1,41 @@
+// import favicon from "./extensions/favicon.png"
+
+const config = {
+  locales: [
+    // 'ar',
+    // 'fr',
+    // 'cs',
+    // 'de',
+    // 'dk',
+    // 'es',
+    // 'he',
+    // 'id',
+    // 'it',
+    // 'ja',
+    // 'ko',
+    // 'ms',
+    // 'nl',
+    // 'no',
+    // 'pl',
+    // 'pt-BR',
+    // 'pt',
+    // 'ru',
+    // 'sk',
+    // 'sv',
+    // 'th',
+    // 'tr',
+    // 'uk',
+    // 'vi',
+    // 'zh-Hans',
+    // 'zh',
+  ],
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config ,
+  bootstrap,
+};
